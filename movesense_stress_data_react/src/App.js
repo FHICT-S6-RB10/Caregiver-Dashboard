5eb8703fe1d269300734fe0312e221ce6a201142
dlalev <dimitar.lalev20@gmail.com>
2022-03-29 10:45:40 +0200

    Improvements

--- movesense_stress_data_react/src/App.js
+++ movesense_stress_data_react/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
+import Grid, { Container, Row, Col } from "react-bootstrap"
 import LeftNav from './LeftNav';
 import RightNav from './RightNav';
 import Home from './Home';
 import Patient from './Patient';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import useFetch from "./useFetch";
+
 
 
 function App() {
@@ -17,21 +19,28 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <Navbar />
+    <Container>
+      <Row md={4}>
+        <Col xs={6} className="maxheight"><Navbar /></Col>
+        <Col xs={6} className="grid-container--fit">
+          <div className='content'>
+            <Switch>
+              <Route exact path ="/">
+                <Home />
+              </Route>
+              <Route path ="/patient/:id">
+                <Patient clients={data}/>
+              </Route>
+            </Switch>
+          </div>
+        </Col>
+        
+      </Row>
+    </Container>
+      
       {/* <RightNav /> */}
       
-      <div className='content'>
-        
-        <Switch>
-          <Route exact path ="/">
-            <Home />
-          </Route>
-          <Route path ="/patient/:id">
-            <Patient clients={data}/>
-          </Route>
-        </Switch>
-        
-      </div>
+      
       
     </div>
     </Router>
