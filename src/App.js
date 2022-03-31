5c0a0a52c4717ab5ab9a225b3290ed0e04fa7eaf
dlalev <dimitar.lalev20@gmail.com>
2022-03-31 12:23:30 +0200

    updated App.js and nodejs.yml

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Navbar from './Navbar';
 import Grid, { Container, Row, Col } from "react-bootstrap"
-import LeftNav from './LeftNav';
-import RightNav from './RightNav';
 import Home from './Home';
 import Patient from './Patient';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
@@ -12,8 +10,7 @@ import useFetch from "./useFetch";
 
 function App() {
 
-  const title = 'Movesense';
-  const { data, isPending, error } = useFetch('https://localhost:44350/patients');
+  const { data } = useFetch('https://localhost:44350/patients');
 
 
   return (
